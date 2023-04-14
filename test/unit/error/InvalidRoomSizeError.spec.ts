8b99a6292674cdb5c02465843897841d99c32b44
Louis <louis-kaan2002@web.de>
2023-04-14 14:32:53 +0200

    style: fixed typo

--- test/unit/error/InvalidRoomSizeError.spec.ts
+++ test/unit/error/InvalidRoomSizeError.spec.ts
@@ -1,7 +1,7 @@
 import InvalidRoomSizeError from '../../../src/error/InvalidRoomSizeError';
 
 describe('InvalidRoomSizeError', () => {
-  test('should create an instance of GameNotFoundError with the correct message and status', () => {
+  test('should create an instance of InvalidRoomSizeError with the correct message and status', () => {
     const errorMessage = 'This is a custom error message';
     const errorStatus = 422;
     const error = new InvalidRoomSizeError(errorMessage);
