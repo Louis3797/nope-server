def9b5baba6deab9f6161aae5129677516350db9
Louis <louis-kaan2002@web.de>
2023-04-25 20:00:20 +0200

    refactor: removed socket handler files

--- src/socket/index.ts
+++
@@ -1,5 +0,0 @@
-import gameQueueHandler from './gameQueueHandler';
-import gameRoomHandler from './gameRoomHandler';
-import tournamentHandler from './tournamentHandler';
-
-export { gameRoomHandler, gameQueueHandler, tournamentHandler };
