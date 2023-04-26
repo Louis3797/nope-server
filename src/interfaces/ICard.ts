c4f956c40204c5cf255fd167b3edcefb040a486a
Louis <louis-kaan2002@web.de>
2023-04-26 12:51:52 +0200

    fix: resolved type error

--- src/interfaces/ICard.ts
+++ src/interfaces/ICard.ts
@@ -14,7 +14,7 @@ export default interface ICard {
     | 'multi'
     | null; // null for action cards or joker
   value: 1 | 2 | 3 | null; // null for action cards or joker
-  select: 1 | -1 | null; // is only not null if the selection card is played
-  selectValue: 1 | 2 | 3 | null;
-  selectedColor: 'red' | 'blue' | 'green' | 'yellow' | null;
+  select?: 1 | -1 | null; // is only not null if the selection card is played
+  selectValue?: 1 | 2 | 3 | null;
+  selectedColor?: 'red' | 'blue' | 'green' | 'yellow' | null;
 }
