9a0676890b86faa3ecab6bde37c01d489e285f97
Louis <louis-kaan2002@web.de>
2023-05-02 22:16:40 +0200

    feat(matchmaking.ts): implemented the rest of the matchmaking algorithm

--- src/socket/matchmaking.ts
+++ src/socket/matchmaking.ts
@@ -30,7 +30,11 @@ export const matchmaking = (
 
     const tournamentData = await prismaClient.tournament.findUnique({
       where: { id: tournamentId },
-      select: { currentSize: true, matches: { select: { id: true } } }
+      select: {
+        currentSize: true,
+        bestOf: true,
+        matches: { select: { id: true } }
+      }
     });
 
     // if tournament was not found than quit
@@ -48,9 +52,9 @@ export const matchmaking = (
     if (queue.size() >= 2) {
       // check if all matches are played
 
-      const player1Priority = queue.frontPriority()!;
+      let player1Priority = queue.frontPriority()!;
       const player1 = queue.dequeue()!;
-      const player2Priority = queue.frontPriority()!;
+      let player2Priority = queue.frontPriority()!;
       const player2 = queue.dequeue()!;
 
       // check if players played a match against each other before
@@ -142,50 +146,62 @@ export const matchmaking = (
             invitationTimeout
           },
           async (err, responses) => {
-            console.log(err);
-            console.log(responses);
-            console.log(responses.length);
-            console.log(responses[0]?.id);
-            console.log(responses[1]?.id);
+            // console.log(err);
+            // console.log(responses);
+            // console.log(responses.length);
+            // console.log(responses[0]?.id);
+            // console.log(responses[1]?.id);
             if (err) {
               // some clients did not acknowledge the event in the given delay
               // check if both does not acknowledge
               // check if only one does not acknowledge
 
-              if (responses.length === 1) {
-                // One client acknowledged the invite
-                // Todo
+              // delete match
+              await prismaClient.match.delete({
+                where: { id: match.id }
+              });
 
-                // if client rejected
-                if (!responses[0]?.accepted) {
-                  // Todo add logic
+              io.to([socketPlayer1.id, socketPlayer2.id]).emit(
+                'match:info',
+                'You and your opponent rejected the invite, therefore both of you will be put back in the matchmaking queue'
+              );
+
+              if (responses.length === 1 && responses[0]?.id) {
+                // One client acknowledged the invite
+                const checkId =
+                  responses[0].id === player1.id
+                    ? player1.id
+                    : responses[0].id === player2.id
+                    ? player2.id
+                    : undefined;
+
+                // check received id
+                if (!checkId) {
+                  queue.enqueue(player1, player1Priority + 1);
+                  queue.enqueue(player2, player2Priority + 1);
                   matchmaking(io, tournamentId);
+                  return;
                 }
 
+                player1Priority += checkId === player1.id ? 1 : 2;
+                player2Priority += checkId === player2.id ? 1 : 2;
+
+                queue.enqueue(player1, player1Priority);
+                queue.enqueue(player2, player2Priority);
                 // client did accept
-                // Todo add logic
               } else {
                 // Both clients did not acknowledge
 
-                // delete match
-                await prismaClient.match.delete({
-                  where: { id: match.id }
-                });
-
-                io.to([socketPlayer1.id, socketPlayer2.id]).emit(
-                  'match:info',
-                  'You and your opponent rejected the invite, therefore both of you will be put back in the matchmaking queue'
-                );
                 // Add the players back to the queue with the same priority
-                queue.enqueue(player1, player1Priority);
-                queue.enqueue(player2, player2Priority);
+                queue.enqueue(player1, player1Priority + 2);
+                queue.enqueue(player2, player2Priority + 2);
                 matchmaking(io, tournamentId);
               }
             } else {
               // if both clients does acknowledge
 
               // check if a client rejected the invitation
-              if (!responses[0]?.accepted && !responses[0]?.accepted) {
+              if (!responses[0]?.accepted && !responses[1]?.accepted) {
                 // both rejected the invitation put them back in the queue
 
                 // delete match
@@ -199,30 +215,28 @@ export const matchmaking = (
                 );
                 // Add the players back to the queue with the same priority
 
-                queue.enqueue(player1, player1Priority);
-                queue.enqueue(player2, player2Priority);
+                queue.enqueue(player1, player1Priority + 1);
+                queue.enqueue(player2, player2Priority + 1);
                 matchmaking(io, tournamentId);
               } else if (
+                // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing
                 (responses[0]?.accepted && !responses[1]?.accepted) ||
                 (!responses[0]?.accepted && responses[1]?.accepted)
               ) {
                 // one client rejected the invite
 
-                const winnerId = responses[0].accepted
+                const winnerId = responses[0]?.accepted
                   ? responses[0].id
                   : responses[1]?.id;
 
-                if (
-                  winnerId !== socketPlayer1.data.user.id &&
-                  winnerId !== socketPlayer2.data.user.id
-                ) {
+                if (winnerId !== player1.id && winnerId !== player2.id) {
                   io.to([socketPlayer1.id, socketPlayer2.id]).emit(
                     'match:info',
                     'An error appeared you will be put back into the matchmaking queue'
                   );
 
-                  queue.enqueue(player1, player1Priority);
-                  queue.enqueue(player2, player2Priority);
+                  queue.enqueue(player1, player1Priority + 1);
+                  queue.enqueue(player2, player2Priority + 1);
                   matchmaking(io, tournamentId);
                   return;
                 }
@@ -236,7 +250,7 @@ export const matchmaking = (
                 });
 
                 // find tournamentStatistic of players for the current tournament
-                const statisticId =
+                const statistic =
                   await prismaClient.tournamentStatistic.findFirst({
                     where: {
                       AND: [
@@ -248,34 +262,36 @@ export const matchmaking = (
                         }
                       ]
                     },
-                    select: { id: true }
+                    select: { id: true, player: { select: { username: true } } }
                   });
 
                 // update the statistic
                 await prismaClient.tournamentStatistic.update({
                   where: {
-                    id: statisticId!.id
+                    id: statistic!.id
                   },
                   data: {
                     wonMatches: { increment: 1 }
                   }
                 });
 
-                queue.enqueue(player1, player1Priority + 1);
-                queue.enqueue(player2, player2Priority + 1);
-
-                // Todo Send client that he has won
-                // ! You was here create match:won emit or so
+                io.to([socketPlayer1.id, socketPlayer2.id]).emit(
+                  'match:info',
+                  `${statistic?.player.username} won the game bc the opponent rejected the match invitation.`
+                );
 
                 // Send tournament info with new score
                 const tInfo = await getTournamentInfo(
                   tournamentId,
-                  'Tournament started'
+                  `${statistic?.player.username} won the game bc the opponent rejected the match invitation.`
                 );
 
                 io.in(`tournament:${tournamentId}`)
                   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
                   .emit('tournament:info', tInfo!);
+
+                queue.enqueue(player1, player1Priority + 1);
+                queue.enqueue(player2, player2Priority + 1);
               } else {
                 await prismaClient.match.update({
                   where: { id: match.id },
@@ -285,9 +301,10 @@ export const matchmaking = (
                 socketPlayer1.join(`tournament-match:${match.id}`);
                 socketPlayer2.join(`tournament-match:${match.id}`);
 
-                // Todo send match begin
-
-                // Todo send players back in queue after their match
+                io.to([socketPlayer1.id, socketPlayer2.id]).emit(
+                  'match:info',
+                  'You accepted the invite. Please wait for the first game to start'
+                );
               }
             }
           }
