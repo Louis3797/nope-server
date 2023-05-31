fa62d8da710356ba360830e2bd18584c8e40f870
Louis <louis-kaan2002@web.de>
2023-05-31 19:07:10 +0200

    feat(util): added sameCardColor

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,17 @@
 import PriorityQueue from './PriorityQueue';
 import compressFilter from './compressFilter.util';
+import indexOfCardInHand from './indexOfCardInHand';
+import isCardInHand from './isCardInHand';
+import sameCardColor from './sameCardColor';
 import { sanitize } from './sanitize.util';
 import shuffle from './shuffle.util';
 
-export { sanitize, compressFilter, shuffle, PriorityQueue };
+export {
+  sanitize,
+  compressFilter,
+  shuffle,
+  PriorityQueue,
+  isCardInHand,
+  sameCardColor,
+  indexOfCardInHand
+};
