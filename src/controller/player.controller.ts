e0141ac7471625fc0cf66927a1c641f0ad73f92c
Louis <louis-kaan2002@web.de>
2023-06-02 13:36:30 +0200

    feat(player.controller): implemented player/{name}/tournament/won route
    Clients can now query all their won tournaments

--- src/controller/player.controller.ts
+++ src/controller/player.controller.ts
@@ -89,10 +89,60 @@ export const getParticipatedTournaments = async (
   res.status(httpStatus.OK).json(playerTournaments);
 };
 
-// export const getWonTournaments = async (
-//   req: Request<{ name: string }>,
-//   res: Response
-// ) => {};
+export const getWonTournaments = async (
+  req: Request<{ name: string }>,
+  res: Response
+) => {
+  const wonTournaments = await prismaClient.player.findUnique({
+    where: {
+      username: req.params.name
+    },
+    select: {
+      wonTournaments: {
+        select: {
+          id: true,
+          createdAt: true,
+          endedAt: true,
+          startedAt: true,
+          status: true,
+          bestOf: true,
+          host: {
+            select: {
+              id: true,
+              username: true,
+              firstname: true,
+              lastname: true
+            }
+          },
+          winner: {
+            select: {
+              id: true,
+              username: true,
+              firstname: true,
+              lastname: true
+            }
+          },
+          players: {
+            select: {
+              id: true,
+              username: true,
+              firstname: true,
+              lastname: true
+            }
+          }
+        }
+      }
+    }
+  });
+
+  if (!wonTournaments) {
+    return res
+      .status(httpStatus.NOT_FOUND)
+      .json({ message: 'Player not found' });
+  }
+
+  res.status(httpStatus.OK).json(wonTournaments);
+};
 
 // export const getHostedTournaments = async (
 //   req: Request<{ name: string }>,
--- src/routes/player.route.ts
+++ src/routes/player.route.ts
@@ -12,7 +12,7 @@ playerRouter.get(
   playerController.getPlayerInfo
 );
 
-playerRouter.get('/:name/stats/', playerController.getStats);
+// playerRouter.get('/:name/stats/', playerController.getStats);
 
 playerRouter.get(
   '/:name/tournaments',
@@ -21,22 +21,22 @@ playerRouter.get(
 
 playerRouter.get('/:name/tournaments/won/', playerController.getWonTournaments);
 
-playerRouter.get(
-  '/:name/tournaments/hosted/',
-  playerController.getHostedTournaments
-);
+// playerRouter.get(
+//   '/:name/tournaments/hosted/',
+//   playerController.getHostedTournaments
+// );
 
-playerRouter.get(
-  '/:name/tournaments/statistics/',
-  playerController.getAllTournamentStatistics
-);
+// playerRouter.get(
+//   '/:name/tournaments/statistics/',
+//   playerController.getAllTournamentStatistics
+// );
 
-playerRouter.get('/:name/matches', playerController.getParticipatedMatches);
+// playerRouter.get('/:name/matches', playerController.getParticipatedMatches);
 
-playerRouter.get('/:name/matches/won/', playerController.getWonMatches);
+// playerRouter.get('/:name/matches/won/', playerController.getWonMatches);
 
-playerRouter.get('/:name/games', playerController.getParticipatedGames);
+// playerRouter.get('/:name/games', playerController.getParticipatedGames);
 
-playerRouter.get('/:name/games/won/', playerController.getWonGames);
+// playerRouter.get('/:name/games/won/', playerController.getWonGames);
 
 export default playerRouter;
