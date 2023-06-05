35714d47d369e90251e6158b529924c6e06a1115
Louis <louis-kaan2002@web.de>
2023-06-05 16:38:25 +0200

    docs: added swagger documentation for /verify-token route

--- src/routes/token.route.ts
+++ src/routes/token.route.ts
@@ -3,8 +3,83 @@ import validate from '../middleware/validate';
 import { verifyTokenSchema } from '../validations/token.validation';
 import { tokenController } from '../controller';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     VerifyTokenCredentials:
+ *       type: object
+ *       properties:
+ *         token:
+ *           type: string
+ *     User:
+ *       type: object
+ *       properties:
+ *         user:
+ *           type: object
+ *           properties:
+ *             id:
+ *               type: string
+ *             username:
+ *               type: string
+ *
+ * /api/verify-token:
+ *   post:
+ *     summary: Verify a token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/VerifyTokenCredentials'
+ *     responses:
+ *       '200':
+ *         description: Token verified successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '400':
+ *         description: Bad Request - Token is required
+ *       '403':
+ *         description: Forbidden - Invalid token
+ */
+
+// Import statements...
+
+/**
+ * @swagger
+ * tags:
+ *   name: Token
+ *   description: Token Verification API
+ */
+
 const tokenRouter = Router();
 
+/**
+ * @swagger
+ * /api/verify-token:
+ *   post:
+ *     tags: [Token]
+ *     summary: Verify a token
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/VerifyTokenCredentials'
+ *     responses:
+ *       '200':
+ *         description: Token verified successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       '400':
+ *         description: Bad Request - Token is required
+ *       '403':
+ *         description: Forbidden - Invalid token
+ */
 tokenRouter.post(
   '/verify-token',
   validate(verifyTokenSchema),
